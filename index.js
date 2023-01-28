7816052f098c5d5d3a3055b0fbcd34262cb0be75
rdx_coder <rahul.dangi.sait@gmail.com>
2023-01-28 18:11:13 +0530

    add a controller and router for home

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-    const express = require('express');
+  
+  const express = require('express');
+   
     const app = express();
+   
     const port = 5555;
 
+//use express router
+
+app.use('/',require('./routes'));
+
+
     app.listen(port,function(err){
-        if(err){
+      
+        if(err)
+        {
             // console.log('error',err);
+          
             console.log(`Erroris runing the server : ${err}`);
+           
             return;
         }
+       
         console.log(`Server is running i the port : ${port}`);
+    
     });
